c65d5f2089344e005f4af8710bec04743f500b85
therexuz <ignavaldecace98@gmail.com>
2021-04-04 23:46:44 -0400

    Formulario final

--- js/index.js
+++ js/index.js
@@ -2,17 +2,22 @@
 var form = document.getElementById("formulario");
 var clearFormBtn = document.getElementById("clear");
 form.addEventListener("submit", function (event) {
-    /*const validateForm: Boolean = validarDatos(form);
-
-    if (validateForm) {
-    }*/
-    var succesMsg = document.body;
-    var img = encodeURIComponent("./img/ok.png");
-    succesMsg.innerHTML =
-        "<h1>hemos recibido sus datos, pronto nos estaremos comunicando con usted</h1>" +
-            "<img src=" +
-            img +
-            ">";
+    var validacion = validarDatos(form);
+    if (validacion) {
+        var succesMsg = document.body;
+        var img = encodeURIComponent("./img/ok.png");
+        succesMsg.innerHTML =
+            "<h1>hemos recibido sus datos, pronto nos estaremos comunicando con usted</h1>" +
+                "<img src=" +
+                img +
+                ">";
+    }
+    else {
+        var errorMsg = document.getElementById("error");
+        errorMsg.innerHTML = "<h3>ERROR: COMPLETE TODOS LOS CAMPOS</h3>";
+        event.preventDefault();
+        return false;
+    }
     event.preventDefault();
 });
 clearFormBtn.addEventListener("click", function (event) {
@@ -27,6 +32,30 @@ function validarDatos(form) {
     var phone = form.elements.namedItem("phone").value;
     var languagesList = form.elements.namedItem("languages");
     var selectedLanguages = [];
+    var experience = form.elements.namedItem("years");
+    var contador = 0;
+    experience.forEach(function (expElements) {
+        var input = expElements;
+        if (input.checked) {
+            contador = contador + 1;
+        }
+    });
+    if (contador < 1)
+        return false;
+    if (firstname == "")
+        return false;
+    if (lastname == "")
+        return false;
+    if (rut == "")
+        return false;
+    var rutValido = validarRut(rut);
+    if (!rutValido)
+        return false;
+    if (email == "")
+        return false;
+    if (phone == "")
+        return false;
+    //Validacion de checkbox//
     languagesList.forEach(function (languageElements) {
         var input = languageElements;
         if (input.checked) {
@@ -34,23 +63,68 @@ function validarDatos(form) {
             selectedLanguages.push(input.value);
         }
     });
-    console.log(firstname);
-    console.log(lastname);
-    console.log(rut);
-    console.log(email);
-    console.log(phone);
+    if (selectedLanguages.length < 1)
+        return false;
+    return true;
 }
+function validarRut(rut) {
+    if (rut == "" || rut == "/\./")
+        return false;
+    var serie = [2, 3, 4, 5, 6, 7, 2, 3];
+    var _a = rut.split("-"), numRut = _a[0], numVerificador = _a[1];
+    var numeros = numRut.split("");
+    var suma = 0;
+    numeros.reverse().map(function (num, i) {
+        suma += Number(num) * serie[i];
+    });
+    var module = suma % 11;
+    var Verificador = 11 - module;
+    var k = "k";
+    var zero = "0";
+    var rutErr = document.getElementById("rutError");
+    if (Verificador == Number(numVerificador)) {
+        console.log("RUT VALIDO");
+        return true;
+    }
+    else if (Verificador == 11) {
+        if (numVerificador != zero) {
+            rutErr.innerHTML = "<p>Rut Invalido</p>";
+            console.log("RUT INVALIDO");
+            return false;
+        }
+        console.log("RUT VALIDO");
+    }
+    else if (Verificador == 10) {
+        if (numVerificador != k) {
+            rutErr.innerHTML = "<p>Rut Invalido</p>";
+            console.log("RUT INVALIDO");
+            return false;
+        }
+        console.log("RUT VALIDO");
+    }
+    else {
+        rutErr.innerHTML = "<p>Rut Invalido</p>";
+        console.log("RUT INVALIDO");
+        return false;
+    }
+    return true;
+}
+var rutInput = document.getElementById("rut");
+rutInput.addEventListener("change", function (event) {
+    var rutErr = document.getElementById("rutError");
+    rutErr.removeChild;
+    event.preventDefault();
+});
 /**
  * Funciones que actualizan el dato a la derecha del Range-slider
  */
 var input = document.getElementById("range");
-console.log(input);
 actualizarInput(input);
 input.addEventListener("input", function (event) {
     actualizarInput(input);
     event.preventDefault();
 });
 function actualizarInput(input) {
-    var span = input.parentElement.querySelector('span');
+    var span = input.parentElement.querySelector("span");
     span.innerHTML = input.value;
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,19 +1,25 @@
 let form = <HTMLFormElement>document.getElementById("formulario");
 let clearFormBtn = <HTMLButtonElement>document.getElementById("clear");
 
-form.addEventListener("submit", function (event: any) {
-    /*const validateForm: Boolean = validarDatos(form);
-
-    if (validateForm) {
-    }*/
-
-    const succesMsg = document.body;
-    const img = encodeURIComponent("./img/ok.png");
-    succesMsg.innerHTML =
-        "<h1>hemos recibido sus datos, pronto nos estaremos comunicando con usted</h1>" +
-        "<img src=" +
-        img +
-        ">";
+form.addEventListener("submit", function (event: Event) {
+    
+    const validacion=validarDatos(form);
+    
+    if(validacion){
+        const succesMsg = document.body;
+        const img = encodeURIComponent("./img/ok.png");
+        succesMsg.innerHTML =
+            "<h1>hemos recibido sus datos, pronto nos estaremos comunicando con usted</h1>" +
+            "<img src=" +
+            img +
+            ">"; 
+    }else{
+        const errorMsg= <HTMLElement>document.getElementById("error")
+        errorMsg.innerHTML="<h3>ERROR: COMPLETE TODOS LOS CAMPOS</h3>"
+        event.preventDefault();
+        return false
+    }
+    
 
     event.preventDefault();
 });
@@ -23,40 +29,108 @@ clearFormBtn.addEventListener("click", function (event: any) {
     event.preventDefault();
 });
 
-function validarDatos(form: HTMLFormElement) {
-    
+function validarDatos(form: HTMLFormElement):boolean {
+   
     const firstname = (<HTMLInputElement>form.elements.namedItem("firstname")).value;
     const lastname = (<HTMLInputElement>form.elements.namedItem("lastname")).value;
     const rut = (<HTMLInputElement>form.elements.namedItem("rut")).value;
-    const email = (<HTMLInputElement>form.elements.namedItem("email")).value;
+    const email = (<HTMLInputElement>form.elements.namedItem("email")).value;    
     const phone = (<HTMLInputElement>form.elements.namedItem("phone")).value;
     const languagesList = <RadioNodeList>form.elements.namedItem("languages");
-
     const selectedLanguages: Array<String>= [];
+    const experience = <RadioNodeList>form.elements.namedItem("years")
+    let contador=0
 
+    experience.forEach(function(expElements){
+        const input =<HTMLInputElement>expElements
+        if(input.checked){
+            contador=contador+1
+        }
+    })
+
+    if(contador<1)return false
+    if(firstname=="") return false;
+    if(lastname=="") return false;
+    if(rut=="") return false;
+    const rutValido:boolean=validarRut(rut);
+    if(!rutValido) return false
+    if(email=="") return false;
+    if(phone=="") return false;
+
+    //Validacion de checkbox//
     languagesList.forEach(languageElements => {
         const input = <HTMLInputElement>languageElements
-
+        
         if(input.checked){
-            console.log(input.value)
+            console.log(input.value);
             selectedLanguages.push(input.value)
         }
     });
 
-    console.log(firstname);
-    console.log(lastname);
-    console.log(rut);
-    console.log(email);
-    console.log(phone);
+    if(selectedLanguages.length<1) return false;
+    return true;
 }
+
+function validarRut(rut:string):boolean{
+    if(rut=="" || rut=="/\./")return false;
+
+    const serie:number[]=[2,3,4,5,6,7,2,3]
+    const [numRut,numVerificador] = rut.split("-")
+    const numeros: string[] = numRut.split("")
+    let suma:number=0;
+
+    numeros.reverse().map(function(num,i){
+        suma += Number(num) * serie[i]
+    })
+
+    const module = suma % 11;
+    const Verificador = 11 - module;
+    
+    const k = "k"
+    const zero = "0"
+    const rutErr=<HTMLElement>document.getElementById("rutError")
+
+    if(Verificador==Number(numVerificador)){
+        console.log("RUT VALIDO");
+        return true
+    }
+    else if(Verificador==11){
+        if(numVerificador!=zero){
+            rutErr.innerHTML="<p>Rut Invalido</p>"
+            console.log("RUT INVALIDO");
+            return false
+        } 
+        console.log("RUT VALIDO");
+
+    }else if(Verificador==10){
+        if(numVerificador!=k){
+            rutErr.innerHTML="<p>Rut Invalido</p>"
+            console.log("RUT INVALIDO");
+            return false
+        } 
+        console.log("RUT VALIDO");
+    }
+    else{
+        rutErr.innerHTML="<p>Rut Invalido</p>"
+        console.log("RUT INVALIDO");
+        return false
+    }
+    return true
+}
+
+const rutInput = <HTMLInputElement>document.getElementById("rut")
+
+rutInput.addEventListener("change",function(event:Event){
+    const rutErr=<HTMLElement>document.getElementById("rutError")
+    rutErr.removeChild
+    event.preventDefault()
+})
 
 /**
  * Funciones que actualizan el dato a la derecha del Range-slider
  */
 
-
 const input: any = document.getElementById("range");
-console.log(input);
 actualizarInput(input);
 
 input.addEventListener("input", function (event: any) {
@@ -65,7 +139,8 @@ input.addEventListener("input", function (event: any) {
 });
 
 function actualizarInput(input: any) {
-    const span = input.parentElement.querySelector('span');
+    const span = input.parentElement.querySelector("span");
     span.innerHTML = input.value;
 }
+
 
